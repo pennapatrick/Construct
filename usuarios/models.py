2fd0cbc66a8fc1656c25198962548496a7511659
Patrick <patrickpennajr@gmail.com>
2023-07-11 21:55:21 -0300

    [FEAT] Create models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Users(AbstractUser):
+    CHOICES_CARGO = (('V', 'Vendedor'),
+                    ('G', 'Gerente'))
+    cargo = models.CharField(max_length=1, choices=CHOICES_CARGO)
