62f295a6c1f38a76bb4f73d76206ec5168b49fba
Patrick <patrickpennajr@gmail.com>
2023-07-11 23:34:48 -0300

    [FEAT] Implementing login and logout views

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rolepermissions.decorators import has_permission_decorator
 from .models import Users
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import auth
 
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
@@ -21,3 +24,25 @@ def cadastrar_vendedor(request):
 
             #TODO: Redirecionar com uma mensagem
         return HttpResponse('Conta criada.')
+    
+def login(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect(reverse('plataforma'))
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        login = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=login, password=senha)
+
+        if not user:
+            #TODO: Redirecionar com mensagem de erro
+            return HttpResponse('Usuário inválido')
+        
+        auth.login(request, user)
+        return HttpResponse('Seja Bem-Vindo!')
+    
+def logout(request):
+    request.session.flush()
+    return redirect(reverse('login'))
