22f7aa50c9df79dd980feb292d6f17dae448e2dd
Patrick <patrickpennajr@gmail.com>
2023-07-11 22:23:07 -0300

    [FEAT] Create urls in usuarios app and Implementing views

---
+++ usuarios/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastrar_vendedor/', views.cadastrar_vendedor, name='cadastrar_vendedor')
+]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rolepermissions.decorators import has_permission_decorator
 
-# Create your views here.
+@has_permission_decorator('cadastrar_vendedor')
+def cadastrar_vendedor(request):
+    return HttpResponse('Teste')
